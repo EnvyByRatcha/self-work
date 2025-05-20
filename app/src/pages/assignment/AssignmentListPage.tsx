987b557d106ff1e85e239061136bdf06fb3da282
RatchanonMskcr <ratchanon.mskcr@gmail.com>
2025-05-20 23:41:39 +0700

    add useAssignment to assignment page

--- app/src/pages/assignment/AssignmentListPage.tsx
+++ app/src/pages/assignment/AssignmentListPage.tsx
@@ -1,4 +1,8 @@
+import useAssignment from "../../hook/assignment.hook";
+
 function AssignmentListPage() {
+  const { assignments } = useAssignment();
+
   return <div>AssignmentListPage</div>;
 }
 
