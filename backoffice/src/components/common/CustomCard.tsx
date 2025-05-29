b5ad225bec469659210ef3d25ca8b5e6f5bc3b52
RatchanonMskcr <ratchanon.mskcr@gmail.com>
2025-05-29 22:36:17 +0700

    add fuc onClick to card

--- backoffice/src/components/common/CustomCard.tsx
+++ backoffice/src/components/common/CustomCard.tsx
@@ -4,17 +4,19 @@ import { Link } from "react-router-dom";
 interface CustomCardProps {
   children: React.ReactNode;
   href?: string;
+  handleClick?: () => void;
 }
 
-function CustomCard({ children, href }: CustomCardProps) {
+function CustomCard({ children, href, handleClick }: CustomCardProps) {
   const cardRender = (
     <Card
+      onClick={handleClick}
       sx={{
         borderRadius: "8px",
         cursor: href ? "pointer" : "default",
       }}
     >
-      <CardContent sx={{ p: 3 }}>{children}</CardContent>
+      <CardContent sx={{ p: "16px 24px" }}>{children}</CardContent>
     </Card>
   );
 
