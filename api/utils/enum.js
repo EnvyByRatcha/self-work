2cf63e27311bf9790f126b18eee7c31c59d08750
RatchanonMskcr <ratchanon.mskcr@gmail.com>
2025-05-29 09:33:57 +0700

    add status in enum

--- api/utils/enum.js
+++ api/utils/enum.js
@@ -2,11 +2,18 @@ const GENERAL_STATUS = [
   "active",
   "inactive",
   "issued",
-  "pending_approval",
+  "reserved",
+  "used",
   "technician-issued",
 ];
 const TRANSITION_TYPES = ["stock-in", "stock-out"];
-const WORKFLOW_STATUS = ["pending", "in_progress", "completed", "cancelled"];
+const WORKFLOW_STATUS = [
+  "pending",
+  "in_progress",
+  "completed",
+  "cancelled",
+  "pending_approval",
+];
 const PART_USAGE_STATUS = ["pending", "used", "returned"];
 
 module.exports = {
