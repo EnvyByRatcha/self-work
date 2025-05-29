566f998a274ea44b4d9c29d33bf94e4fc691372b
RatchanonMskcr <ratchanon.mskcr@gmail.com>
2025-05-29 09:31:32 +0700

    create custom modal component

---
+++ app/src/components/Modal/CustomModal.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+interface CustomModalProps {
+  title: String;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  children: React.ReactNode;
+}
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  outline: "solid 2px",
+  outlineColor: "outline.color",
+  borderRadius: "8px",
+  boxShadow: 24,
+  p: 4,
+};
+
+const CustomModal = ({ title, open, setOpen, children }: CustomModalProps) => {
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button
+        sx={{
+          bgcolor: "custom.linkButton",
+          padding: "8px 16px",
+          borderRadius: "8px",
+          "&:hover": {
+            bgcolor: "custom.linkButtonHover",
+          },
+          textTransform: "none",
+          color: "text.secondary",
+        }}
+        onClick={() => setOpen(true)}
+      >
+        <Typography fontSize={"14px"} fontWeight={600}>
+          {title}
+        </Typography>
+      </Button>
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={style}>
+          <Typography
+            fontWeight={700}
+            fontSize={"14px"}
+            color="text.primary"
+            mb={2}
+          >
+            {title}
+          </Typography>
+          {children}
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default CustomModal;
---
+++ app/src/components/Modal/CustomModalV2.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+interface CustomModalV2Props {
+  title: String;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  children: React.ReactNode;
+}
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  minWidth: 400,
+  bgcolor: "background.paper",
+  outline: "solid 2px",
+  outlineColor: "outline.color",
+  borderRadius: "8px",
+  boxShadow: 24,
+  p: 4,
+};
+
+const CustomModalV2 = ({ title, open, setOpen, children }: CustomModalV2Props) => {
+  const handleClose = () => setOpen(false);
+
+  return (
+    <Modal open={open} onClose={handleClose}>
+      <Box sx={style}>
+        <Typography
+          fontWeight={700}
+          fontSize={"14px"}
+          color="text.primary"
+          mb={2}
+        >
+          {title}
+        </Typography>
+        {children}
+      </Box>
+    </Modal>
+  );
+};
+
+export default CustomModalV2;
