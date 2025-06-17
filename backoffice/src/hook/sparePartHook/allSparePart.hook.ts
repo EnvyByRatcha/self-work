eb3e190518ca7eb378d52bccf6dfe51b035d16f0
RatchanonMskcr <ratchanon.mskcr@gmail.com>
2025-06-17 12:17:20 +0700

    create file spare part hook
    - create spare-part hook
    - create all spare part hook

---
+++ backoffice/src/hook/sparePartHook/allSparePart.hook.ts
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { SparePartUnit } from "../../interface/ISparePart";
+import { useDebounce } from "../useDebounced.hook";
+import useSparePartUnit from "../sparePartUnit.hook";
+
+const useAllSparePartUnit = (sparePartId: string) => {
+  const { getSparePartUnitById } = useSparePartUnit();
+
+  const [sparePartUnits, setSparePartUnits] = useState<SparePartUnit[]>([]);
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const debouncedSearchTerm = useDebounce(searchTerm, 800);
+  const [statusFilter, setStatusFilter] = useState("all");
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
+  const [limit, setLimit] = useState(5);
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetchSparePartUnit();
+  }, [sparePartId, currentPage, limit, debouncedSearchTerm, statusFilter]);
+
+  const fetchSparePartUnit = async () => {
+    setLoading(true);
+    setError(null);
+    if (!sparePartId) return;
+    try {
+      const data = await getSparePartUnitById(
+        sparePartId,
+        currentPage,
+        limit,
+        debouncedSearchTerm,
+        statusFilter
+      );
+      if (data?.success) {
+        setSparePartUnits(data.data.sparePartUnits);
+        setTotalPage(data.data.pagination.totalPage);
+      }
+    } catch (error) {
+      setError("fail to fetching sparepart units");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    sparePartUnits,
+    searchTerm,
+    setSearchTerm,
+    setStatusFilter,
+    currentPage,
+    totalPage,
+    setCurrentPage,
+    setLimit,
+    loading,
+    error,
+  };
+};
+
+export default useAllSparePartUnit;
---
+++ backoffice/src/hook/sparePartHook/sparePart.hook.ts
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import sparePartService from "../../service/sparePartService";
+import type { SparePart, SparePartFormData } from "../../interface/ISparePart";
+import { unwrapOrError } from "../../utils/upwrapOrError";
+
+const useSparePart = () => {
+  const [spareParts, setSpareParts] = useState<SparePart[]>([]);
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [statusFilter, setStatusFilter] = useState("all");
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
+  const [limit, setLimit] = useState(5);
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetchSparePart(currentPage, limit, searchTerm, statusFilter);
+  }, [currentPage, searchTerm, statusFilter]);
+
+  const fetchSparePart = async (
+    page: number,
+    limit: number,
+    search?: string,
+    status?: string
+  ) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const data = await sparePartService.getAllSparePart(
+        page,
+        limit,
+        search,
+        status
+      );
+      const result = unwrapOrError(data);
+      setSpareParts(result.data.spareParts);
+      setTotalPage(result.data.pagination.totalPage);
+    } catch (error) {
+      setError("fail to fetching sparePart");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getSparePartById = async (id: string) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const data = await sparePartService.getSparePartById(id);
+      const result = unwrapOrError(data);
+      if (result.success) {
+        return result;
+      }
+    } catch (error) {
+      setError("fail to fetching sparePart with ID");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const createSparePart = async (payload: SparePartFormData) => {
+    const data = await sparePartService.createSparePart(payload);
+    return data;
+  };
+
+  return {
+    spareParts,
+    setSearchTerm,
+    setStatusFilter,
+    getSparePartById,
+    createSparePart,
+    totalPage,
+    currentPage,
+    setCurrentPage,
+    setLimit,
+    loading,
+    error,
+  };
+};
+
+export default useSparePart;
